b9f4a6df5058b2dca99e3eece4f9daeb22dee09a
natnic17 <nbarejka@gmail.com>
2020-12-22 21:24:31 +0300

    test2

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from core.models import *
-from core.views import get_full_data, get_account_data
+from core.views import get_full_data, get_account_data, get_currency_name_with_code
 
 class CoreTest(TestCase):
 	"""docstring for ClassName"""
@@ -11,3 +11,7 @@ class CoreTest(TestCase):
 	def test_get_account_data(self):
 		a = Account.objects.get(name='Мой счет')
 		self.assertEqual(get_account_data(a.id)['total'], 1000)
+
+	def test_currency_name_with_code(self):
+		currency = Currency.objects.get(name='Доллары')
+		self.assertEqual(get_currency_name_with_code(currency.id), 'Доллары $')
